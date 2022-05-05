5a4fba0399ca861825ca04b2105983e1801c1874
Silvio Arem <silvio.arem@gmail.com>
2022-05-05 17:28:40 -0300

    Mapeando, criando as classes com relação ao contexto do projeto

---
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -0,0 +1,27 @@
+package com.dio.santander.bankline.api.model;
+
+public class Correntista {
+	private Integer id;
+	private String cpf;
+	private String nome;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
