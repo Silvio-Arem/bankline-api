9ee68ba9804213c00ac2d74919ba8b16058750c3
Silvio Arem <silvio.arem@gmail.com>
2022-05-05 23:46:44 -0300

    criamos as classes de modelo da aplicacao

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -4,6 +4,15 @@ public class Correntista {
 	private Integer id;
 	private String cpf;
 	private String nome;
+	
+	private Conta conta;
+	
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 	public Integer getId() {
 		return id;
 	}
